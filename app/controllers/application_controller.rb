be621a182ef57ecd5858b367893b6fd75263a606
Sawood Alam <ibnesayeed@gmail.com>
2010-09-26 01:57:47 -0400

    Site Description Added.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  before_filter init_vars
+
+  def init_vars
+    @site_desc = "This comic (komik) series created by Manish Wadhwa (aka M or Dr.M) is the komik with a 'K'. The komik of Me, You and the big 'K' of life. Now what's the big 'K', that's a mystery."
+  end
 
   private
 
