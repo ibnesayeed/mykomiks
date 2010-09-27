7c7627a1c942a4a2ee607b73dec744e9ec743301
Sawood Alam <ibnesayeed@gmail.com>
2010-09-26 23:02:03 -0400

    Commets Sorted.

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -4,7 +4,7 @@ class CommentsController < ApplicationController
   # GET /comments
   # GET /comments.xml
   def index
-    @comments = Comment.all
+    @comments = Comment.order("id DESC")
 
     respond_to do |format|
       format.html # index.html.erb
